29a89b9c52c9ae25936e43c888194144b6ddcd47
SanobarTamboli <sanobartamboli05@gmail.com>
2026-01-30 15:43:26 +0530

    UC1 Complete with Test Cases

--- src/main/java/com/apps/quantityMeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantityMeasurement/QuantityMeasurementApp.java
@@ -1,4 +1,45 @@
 package com.apps.quantityMeasurement;
 
 public class QuantityMeasurementApp {
+    public static class Feet {
+        private final double value;
+
+        public Feet (double value) {
+
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null || getClass() !=obj.getClass()){
+                return false;
+            }
+            Feet other = (Feet) obj;
+            return Double.compare(other.value, this.value) ==0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.valueOf(value).hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return value+ "feet";
+        }
+    }
+
+    public static void  main (String [] args){
+        Feet f1 = new Feet(1.5);
+        Feet f2 = new Feet(1.5);
+        Feet f3 = new Feet(0.5);
+
+        System.out.println("Are feet1 and feet2 equal? " + f1.equals(f2));
+        System.out.println("Are feet1 and feet2 equal? " + f1.equals(f3));
+        System.out.println("Are feet1 and null equal? " + f1.equals(null));
+    }
 }
--- src/test/java/com/apps/QuantityMeasurement/test/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/QuantityMeasurement/test/QuantityMeasurementAppTest.java
@@ -1,4 +1,43 @@
 package com.apps.QuantityMeasurement.test;
+import com.apps.quantityMeasurement.QuantityMeasurementApp.Feet;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class QuantityMeasurementAppTest {
+
+    @Test
+    public void testFeetEquality_SameValue() {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+        assertEquals(f1, f2, "Double value should be exactly equal");
+    }
+
+    @Test
+    public void testFeetEquality_DifferentValue() {
+        Feet f1 = new Feet(0.0);
+        Feet f2 = new Feet(1.0);
+        assertNotEquals(f1, f2, "Double value should not be equal");
+    }
+
+    @Test
+    public void testFeetEquality_NullComparison() {
+        Feet f1 = new Feet(0.0);
+        assertNotEquals(null, f1, "Double value should not be equal to null");
+    }
+
+    @Test
+    public void testFeetEquality_DifferentClass() {
+        Feet f1 = new Feet(0.0);
+        Feet f2 = new Feet(1);
+        assertNotEquals(f1, f2, "Double value should not be equal");
+    }
+
+    @Test
+    public void testFeetEquality_SameReference() {
+        Feet f1 = new Feet(5.0);
+        assertEquals(f1, f1, "Value should be equal to itself");
+    }
 }
